e14a540a90aafc00aa844123416698281f7da41f
fareedkamal <fkamalars@gmail.com>
2024-04-16 22:48:03 +0500

    img path fixed

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,20 +1,20 @@
-import { useMediaQuery } from "@mui/material";
+import { useMediaQuery } from '@mui/material';
 
 const HotelCard = ({ img, title }) => {
-  const md = useMediaQuery("(max-width : 900px)");
+  const md = useMediaQuery('(max-width : 900px)');
   return (
     <div
-      className={`${md ? "w-full h-[200px]" : "h-[300px] w-[600px]"}
+      className={`${md ? 'w-full h-[200px]' : 'h-[300px] w-[600px]'}
         flex items-end rounded-2xl p-[2em]`}
       style={{
         background: `linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.50)), url(${img})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
       }}
     >
       <div>
-        <h1 className="text-[30px] font-medium text-white">{title}</h1>
-        <button className="bg-white rounded-3xl px-[18px] py-[8px]">
+        <h1 className='text-[30px] font-medium text-white'>{title}</h1>
+        <button className='bg-white rounded-3xl px-[18px] py-[8px]'>
           View Hotel
         </button>
       </div>
@@ -23,22 +23,22 @@ const HotelCard = ({ img, title }) => {
 };
 
 const Discover = () => {
-  const sm = useMediaQuery("(max-width : 600px)");
-  const md = useMediaQuery("(max-width : 900px)");
+  const sm = useMediaQuery('(max-width : 600px)');
+  const md = useMediaQuery('(max-width : 900px)');
   return (
-    <div className="w-full bg-gray-100">
-      <div className="w-[300px] sm:w-[500px] md:w-[800px] lg:w-[1100px] m-auto py-[5em]">
-        <div className="mb-[1em]">
+    <div className='w-full bg-gray-100'>
+      <div className='w-[300px] sm:w-[500px] md:w-[800px] lg:w-[1100px] m-auto py-[5em]'>
+        <div className='mb-[1em]'>
           <h1
             className={`${
-              sm ? "text-[15px]" : md ? "text-[20px]" : "text-[30px]"
+              sm ? 'text-[15px]' : md ? 'text-[20px]' : 'text-[30px]'
             } font-medium`}
           >
             Discover Our Newest Hotels
           </h1>
           <p
             className={`${
-              sm ? "text-[10px]" : md ? "text-[15px]" : "text-[20px]"
+              sm ? 'text-[10px]' : md ? 'text-[15px]' : 'text-[20px]'
             } `}
           >
             Planning a getaway? Explore our latest properties around the globe,
@@ -48,11 +48,17 @@ const Discover = () => {
         </div>
         <div
           className={`${
-            md ? "flex-col" : "flex-row"
+            md ? 'flex-col' : 'flex-row'
           } flex items-center gap-[1em]`}
         >
-          <HotelCard img="/hotel_3.jpg" title="JW Marriot Masai Lodge" />
-          <HotelCard img="/hotel_5.jpg" title="Moxy Brooklyn Williamsburg" />
+          <HotelCard
+            img='/reservation-app/hotel_3.jpg'
+            title='JW Marriot Masai Lodge'
+          />
+          <HotelCard
+            img='/reservation-app/hotel_5.jpg'
+            title='Moxy Brooklyn Williamsburg'
+          />
         </div>
       </div>
     </div>
